04e5b8a2225349774ca2bf6ae092bd350112a8ab
Steven G. Harms <steven@devbootcamp.com>
2014-08-28 14:24:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twooter::Application.config.secret_token = '77576652c19431f1221affac2f392b843f221e121413716e96ec92db8c1be5a702c6dba7a88f3bd10cf54fb4a1a1cf3a5c9f8850e1a4d0984eee4776a1c6afd9'
