b8f4bf4c960ae3ff53293100a6c930dc5e1e64b6
Steven G. Harms <steven@devbootcamp.com>
2014-08-29 04:51:28 -0700

    Enable Twoots#show as a JS.erb-serving endpoint

--- app/controllers/twoots_controller.rb
+++ app/controllers/twoots_controller.rb
@@ -17,6 +17,7 @@ class TwootsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.js
       format.json { render json: @twoot }
     end
   end
